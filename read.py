1b15800ac957396a777ed653e522b04ba9955273
Shaun Lee <yngwieli10@gmail.com>
2019-02-09 15:32:04 +0800

    forth version calc avg

--- read.py
+++ read.py
@@ -6,5 +6,13 @@ with open('reviews.txt', 'r') as f:
         data.append(line)
         if count % 1000000 == 0 :
             print(len(data))
+print('總共有', len(data), '筆資料')        
 
-        
+sum_len = 0
+for d in data:
+    sum_len += len(d)
+print('每筆留言平均長度是', sum_len/len(data))
+
+
+
+    
