05471a1e5aefba580b07c031d3e02de2ddc4a1fb
Shaun Lee <yngwieli10@gmail.com>
2019-02-09 14:58:41 +0800

    third vesion

--- read.py
+++ read.py
@@ -4,7 +4,7 @@ with open('reviews.txt', 'r') as f:
     for line in f:
         count += 1
         data.append(line)
-        if count % 1000 == 0 :
+        if count % 1000000 == 0 :
             print(len(data))
 
         
